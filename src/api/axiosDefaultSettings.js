44f3d7f6b1e19ec9ad07e97a695048a5eea03f09
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-08-02 21:22:26 -0400

    Replace baseURL with an environment variable

--- src/api/axiosDefaultSettings.js
+++ src/api/axiosDefaultSettings.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/api/";
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
 axios.defaults.withCredentials = true;
 
 export const refresh_api = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
 
 export const main_api = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
