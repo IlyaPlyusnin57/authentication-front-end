c4d1e127d0cd374505eed19324a6c5b2460b9787
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-08-03 19:30:23 -0400

    Remove commented code
    await handleDelete function and pass the api argument to it

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,36 +1,24 @@
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useAuth } from "./context/AuthContext";
 import { ApiCalls } from "./api/ApiCalls";
+import useAxiosConfig from "./api/useAxiosConfig";
 
 function Post({ post, setPosts }) {
   const {
     userInfo: { user },
   } = useAuth();
 
-  // async function handleDelete() {
-  //   try {
-  //     const res = await axios.delete(`posts/delete/${post._id}`, {
-  //       data: { userId: user._id },
-  //       headers: { authorization: `Bearer ${user.accessToken}` },
-  //     });
-
-  //     if (res.data) {
-  //       setPosts((prev) => prev.filter((p) => p._id !== post._id));
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+  const api = useAxiosConfig();
 
   return (
     <div className="post">
       <div className="delete-icon-container">
         <DeleteIcon
           className="delete-icon"
-          onClick={() => {
-            const data = ApiCalls.handleDelete(post, user);
+          onClick={async () => {
+            const res = await ApiCalls.handleDelete(api, post, user);
 
-            if (data) {
+            if (res.status === 200) {
               setPosts((prev) => prev.filter((p) => p._id !== post._id));
             }
           }}
