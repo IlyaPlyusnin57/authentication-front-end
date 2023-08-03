514d6073ffb68412b811d962245c3c2fe2fed97e
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-08-03 19:36:35 -0400

    Import useAxiosConfig to create api variable
    Pass api to createPost
    
    Refactor code to createNewPost function and call it when user hits Enter
    or presses Create

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,34 +1,22 @@
 import "./new-post.scss";
 import { useRef } from "react";
 import { ApiCalls } from "../api/ApiCalls";
-import { useAuth } from "../context/AuthContext";
+import useAxiosConfig from "../api/useAxiosConfig";
 
-function NewPost({ setPosts, userInfo: { user } }) {
+function NewPost({ setPosts, user }) {
   const postText = useRef(null);
-  const { setUserInfo } = useAuth();
+  const api = useAxiosConfig();
 
-  // async function createPost() {
-  //   try {
-  //     const { data: post } = await axios.post(
-  //       "posts/post",
-  //       {
-  //         userId: user._id,
-  //         text: postText.current.value,
-  //       },
-  //       {
-  //         headers: { authorization: `Bearer ${user.accessToken}` },
-  //       }
-  //     );
-  //     setPosts((prev) => [...prev, post]);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+  async function createNewPost() {
+    const post = await ApiCalls.createPost(api, postText.current.value, user);
+    setPosts((prev) => [...prev, post]);
+    postText.current.value = "";
+  }
 
-  function handleOnKeyDown(e) {
+  async function handleOnKeyDown(e) {
     if (e.key === "Enter") {
       e.preventDefault();
-      //apiCalls.createPost(postText, setPosts);
+      await createNewPost();
     }
   }
 
@@ -40,13 +28,7 @@ function NewPost({ setPosts, userInfo: { user } }) {
         ref={postText}
         onKeyDown={handleOnKeyDown}
       />
-      <button
-        type="submit"
-        onClick={async () => {
-          const post = await ApiCalls.createPost(postText, user);
-          setPosts((prev) => [...prev, post]);
-        }}
-      >
+      <button type="submit" onClick={createNewPost}>
         Create
       </button>
     </div>
