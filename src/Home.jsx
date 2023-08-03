7f31d946ef4fa93ce477ec2b8e85da7f75ed2fab
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-08-03 19:27:13 -0400

    Remove unnecessary code

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,10 @@
 import { useAuth } from "./context/AuthContext";
 import Header from "./components/Header";
 import Navbar from "./Navbar";
-import { useEffect } from "react";
-import axiosConfig from "./api/axiosConfig";
-import axios from "axios";
 import Posts from "./Posts";
 
 function Home() {
-  const { userInfo, setUserInfo } = useAuth();
-
-  // useEffect(() => {
-  //   const interceptor = axiosConfig(userInfo.user, setUserInfo);
-  //   return () => axios.interceptors.request.eject(interceptor);
-  // }, []);
+  const { userInfo } = useAuth();
 
   return (
     <div>
