dfcde8375e5bba5eb3c9266ea614728e589dfa2b
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-08-02 21:20:11 -0400

    Rewrite axiosConfig as a hook
    This hook attaches an interceptor to main_api and then returns it

---
+++ src/api/useAxiosConfig.js
@@ -0,0 +1,53 @@
+import { isExpired } from "react-jwt";
+import { ApiCalls } from "./ApiCalls";
+import { useEffect } from "react";
+import { refresh_api, main_api } from "./axiosDefaultSettings";
+import { useAuth } from "../context/AuthContext";
+
+async function refreshToken(user, setUserInfo) {
+  try {
+    const res = await refresh_api.post("auth/refresh", {
+      userId: user._id,
+    });
+
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    console.log("SHOULD BE LOGGING OUT!");
+    await ApiCalls.logout(user, setUserInfo);
+  }
+}
+
+export default function useAxiosConfig() {
+  const {
+    userInfo: { user },
+    setUserInfo,
+  } = useAuth();
+
+  useEffect(() => {
+    const axiosInterceptor = main_api.interceptors.request.use(
+      async (config) => {
+        const accessToken = sessionStorage.getItem("accessToken");
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
+
+        if (isExpired(accessToken)) {
+          console.log("it was expired");
+          const token = await refreshToken(user, setUserInfo);
+          sessionStorage.setItem("accessToken", token.accessToken);
+          config.headers["Authorization"] = `Bearer ${token.accessToken}`;
+        }
+
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      main_api.interceptors.request.eject(axiosInterceptor);
+    };
+  }, []);
+
+  return main_api;
+}
